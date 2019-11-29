bd2e3ee38b997f3882683562cc64d92e0f0914b5
Anton <parfenuk.anton@gmail.com>
2019-11-29 20:05:00 +0300

    Task 1st complete.

---
+++ TestJob_Template/Assets/AnimController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimController : MonoBehaviour
+{
+    private Animator _animator;
+
+    private bool Wind = false;
+    private void Awake()
+    {
+        _animator = GetComponent<Animator>();
+        _animator.Play("withoutWind", -1, Random.Range(0.0f, 1.0f));
+    }
+
+    public void WindTriger()
+    {
+        Wind = !Wind;
+        _animator.SetBool("Wind", Wind);
+    }
+}
